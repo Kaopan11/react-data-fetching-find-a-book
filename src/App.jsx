bfa446b14e0b6588c380e98c5e48d7089ef21a10
Peerawat (kaopan) <atkaew@gmail.com>
2026-07-02 15:58:37 +0700

    feat(app): implement book search with Open Library API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+
+const [searchText, setSearchText] = useState("");
+const [books, setBooks] = useState([]);
+
+useEffect(() => {
+  if (!searchText) {
+    setBooks([]);
+    return;
+  }
+
+  const url = `https://openlibrary.org/search.json?title=${encodeURIComponent(searchText)}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      setBooks(data.docs ?? []);
+    });
+}, [searchText]);
+
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <input 
+        type="text"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      <uL>
+        {books.map((book) => (
+          <li key={book.key}>
+            {book.title}
+          </li>
+        ))}
+      </uL>
+    </div>
+
+
+  );
 }
 
 export default App;
